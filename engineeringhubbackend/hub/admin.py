280535a8bcd4ed8ddb86264db246713af96752fe
Michael Navazhylau <mankbf@umsystem.edu>
2021-06-17 12:08:41 -0500

    Added instructor model, student model

--- engineeringhubbackend/hub/admin.py
+++ engineeringhubbackend/hub/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Major, College, Department
+from .models import Major, College, Department, Student, Instructor
 
 admin.site.register(Major)
 
 admin.site.register(College)
 
 admin.site.register(Department)
+
+admin.site.register(Student)
+
+admin.site.register(Instructor)
--- engineeringhubbackend/hub/models.py
+++ engineeringhubbackend/hub/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Major(models.Model):
@@ -19,3 +21,11 @@ class College(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    yearsInSchool = models.IntegerField()
+
+class Instructor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # TBD
