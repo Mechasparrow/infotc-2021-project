d8feccf8d54fb1b1e9e4e6ecc685533765e585b6
Michael Navazhylau <mankbf@umsystem.edu>
2021-07-10 00:12:56 -0500

    able to search users

--- engineeringhubfrontend/src/app/services/api/api.service.ts
+++ engineeringhubfrontend/src/app/services/api/api.service.ts
@@ -120,4 +120,17 @@ export class ApiService {
 
     return this.http.get(`${this.apiEndpoint}/projects/searchProjects/`, httpOptions).toPromise();
   }
+
+  async searchUsers(searchString:string){
+    var payload = 
+    {
+      "search_query": searchString  
+    }
+
+    var httpOptions = {
+      params: payload
+    }
+
+    return this.http.get(`${this.apiEndpoint}/users/searchUsers/`, httpOptions).toPromise();
+  }
 }
--- engineeringhubfrontend/src/app/share/main-search/main-search.component.ts
+++ engineeringhubfrontend/src/app/share/main-search/main-search.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Disclipline } from 'src/app/models/Disclipline';
 import { Project } from 'src/app/models/Project';
 import { Skill } from 'src/app/models/Skill';
+import { User } from 'src/app/models/User';
 import { ApiService } from 'src/app/services/api/api.service';
 
 import { CardType } from '../result-card/result-card.component';
@@ -10,6 +11,7 @@ import { CardType } from '../result-card/result-card.component';
 interface SearchResult {
   type: CardType,
   name: string,
+  genericName: string,
   description: string,
   attributes: string[]
 }
@@ -34,18 +36,60 @@ export class MainSearchComponent implements OnInit {
 
   }
 
+  async projectSearch(searchQuery: string){
+
+    let projectSearchResults = <Project[]> (await this.api.searchProjects(searchQuery));
+
+    this.searchResults = (<Project[]>projectSearchResults).map ((projectResult: Project) => {
+      return <SearchResult>{
+        type: CardType.ProjectView,
+        name: projectResult.name,
+        description: projectResult.description,
+        attributes: projectResult.project_discliplines.map((disclipline:Disclipline) => disclipline.name)
+      }
+    });
+  }
+
+  async userSearch(searchQuery: string){
+    let userSearchResults = <User[]> (await this.api.searchUsers(searchQuery));
+
+    this.searchResults = (<User[]>userSearchResults).map ((userSearchResult: User) => {
+      return <SearchResult>{
+        type: CardType.UserView,
+        genericName: userSearchResult.username
+      }
+    });
+  }
+
+  async groupSearch(searchQuery:string){
+    alert("not implemented");
+  }
+
+  async projectProposalSearch(searchQuery:string){
+    alert("not implemented");
+  }
+
   async onSubmit(){
-    if (this.searchForm.get("searchFilter")?.value == "projects"){
+    if (this.searchForm.get("searchFilter")?.value != null){
+      let searchFilter = this.searchForm.get("searchFilter")?.value;
       let searchQuery = this.searchForm.get("searchInput")?.value;
-      let projectSearchResults = <Project[]> (await this.api.searchProjects(searchQuery));
-      this.searchResults = (<Project[]>projectSearchResults).map ((projectResult: Project) => {
-        return <SearchResult>{
-          type: CardType.ProjectView,
-          name: projectResult.name,
-          description: projectResult.description,
-          attributes: projectResult.project_discliplines.map((disclipline:Disclipline) => disclipline.name)
-        }
-      });
+      
+      console.log({filter: searchFilter})
+
+      switch (searchFilter){
+        case "projects":
+          await this.projectSearch(searchQuery);
+          return;
+        case "project-proposals":
+          await this.projectProposalSearch(searchQuery);
+          return;
+        case "users":
+          await this.userSearch(searchQuery);
+          return;
+        case "groups":
+          await this.groupSearch(searchQuery);
+          return;
+      }
     }
   }
 
