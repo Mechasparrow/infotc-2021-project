d8d7fbf596825cd72b4502f2078c5b933547ff4d
Michael Navazhylau <mankbf@umsystem.edu>
2021-07-12 18:23:51 -0500

    browsing scaffold

--- engineeringhubfrontend/src/app/app-routing.module.ts
+++ engineeringhubfrontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddNewProjectComponent } from './pages/add-new-project/add-new-project.component';
 import { AddProjectNotePageComponent } from './pages/add-project-note-page/add-project-note-page.component';
 import { AddProjectProposalPageComponent } from './pages/add-project-proposal-page/add-project-proposal-page.component';
+import { BrowsePageComponent } from './pages/browse-page/browse-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
@@ -12,6 +13,7 @@ import { ViewProjectPageComponent } from './pages/view-project-page/view-project
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
+  {path: 'browse', component: BrowsePageComponent},
   {path: 'login', component: LoginPageComponent},
   {path: 'signup', component: SignUpPageComponent},
   {path: 'profile', component: ProfilePageComponent},
