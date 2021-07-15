2616b53f24c650b0a0dbfdea56e28a4d8cf63b8c
Michael Navazhylau <mankbf@umsystem.edu>
2021-07-15 11:10:48 -0500

    working on add event page

--- engineeringhubfrontend/src/app/pages/add-event-page/add-event-page.component.ts
+++ engineeringhubfrontend/src/app/pages/add-event-page/add-event-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import * as moment from 'moment';
 import { formatDate } from '@angular/common';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 //https://newbedev.com/how-can-i-set-my-reactive-form-date-input-value
 
@@ -11,6 +12,18 @@ import { formatDate } from '@angular/common';
   styleUrls: ['./add-event-page.component.scss']
 })
 export class AddEventPageComponent implements OnInit {
+
+  //formatDate(this.post.startDate, 'yyyy-MM-dd', 'en')
+
+  groupEventForm: FormGroup = new FormGroup({
+    name: new FormControl("", Validators.required),
+    description: new FormControl("", Validators.required),
+    location: new FormControl("" ,Validators.required),
+    "timestart-date": new FormControl("", Validators.required),
+    "timeend-date": new FormControl("", Validators.required),
+    "timestart-time": new FormControl("", Validators.required),
+    "timeend-time": new FormControl("", Validators.required)
+  });
 
   constructor() { }
 
@@ -23,6 +36,10 @@ export class AddEventPageComponent implements OnInit {
     let parsedDate = moment(dateString, "YYYY-MM-DD hh:mm").toDate();
 
     return parsedDate;
+  }
+
+  async onSubmit(){
+
   }
 
 }
