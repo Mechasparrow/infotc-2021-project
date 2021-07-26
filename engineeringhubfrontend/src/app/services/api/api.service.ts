5f8bba5b1ab9cb4622f7a82afe301de85775ed0a
Michael Navazhylau <mankbf@umsystem.edu>
2021-07-25 19:57:16 -0500

    code change

--- engineeringhubfrontend/src/app/services/api/api.service.ts
+++ engineeringhubfrontend/src/app/services/api/api.service.ts
@@ -20,7 +20,7 @@ import { connectableObservableDescriptor } from 'rxjs/internal/observable/Connec
 export class ApiService {
   
 
-  private apiEndpoint: string = "http://23.101.131.209:8000/api/hub";
+  private apiEndpoint: string = "http://20.106.154.30:8000/api/hub";
 
   constructor(private http: HttpClient, private tokenStore: TokenStoreService) {
 
